4f0fa7f6b2983db1aa115bfd16d57417a2705edb
Angelo <angeloghiotto@gmail.com>
2023-06-15 11:36:45 -0300

    Sorting reviews

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -53,7 +53,11 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
-        return view('books.show', ['book' => $book]);
+        return view('books.show', ['book' => $book->load(
+            [
+                'reviews' => fn ($query) => $query->latest()
+            ]
+        )]);
     }
 
     /**
